ee030b1b9cc667f5149127591669bba9e541fde3
Fluxiaz <mgamads@gmail.com>
2021-08-19 09:53:38 +0200

    projekt

---
+++ MN Groop A.P.S/ReadMe.md
@@ -0,0 +1,8 @@
+﻿# Logbog 
+
+# Mandag 31/05-2021
+	
+	# Mads 
+
+
+	# Niklas
---
+++ MN Groop A.P.S/Repositories/Candy.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MN_Groop_A.P.S.Repositories
+{
+    public class Candy : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
