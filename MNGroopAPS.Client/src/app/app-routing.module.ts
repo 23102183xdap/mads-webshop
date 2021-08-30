6efb2a6f8d17e4b2a428073e95f0ca191d7cfa34
Fluxiaz <mgamads@gmail.com>
2021-08-30 10:03:16 +0200

    routerr links

--- MNGroopAPS.Client/src/app/app-routing.module.ts
+++ MNGroopAPS.Client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BolcherComponent } from './bolcher/bolcher.component';
 import { KategoriComponent } from './kategori/kategori.component';
+import { LakridsBolcherComponent } from './lakrids-bolcher/lakrids-bolcher.component';
 
 const routes: Routes = [
 
@@ -20,8 +21,13 @@ const routes: Routes = [
   {path: 'kvug', component:KvugComponent},
 
   // {path: '' , redirectTo: '/bolcher', pathMatch: 'full'},
-  {path: 'bolcher', component:BolcherComponent}
+  {path: 'bolcher', component:BolcherComponent},
   
+  {path: 'lakrids-bolcher', component:LakridsBolcherComponent},
+
+  {path: 'sure-bolcher', component:LakridsBolcherComponent},
+
+  {path: 'blandet-bolcher', component:LakridsBolcherComponent}
   
 
 ];
